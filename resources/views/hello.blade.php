eaebc07738769ba551305110d5abbf3b59ff805c
meowmeowxw <giovanni.disanti@protonmail.com>
2020-12-22 22:14:25 +0100

    Add hello view

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Title </title>
+</head>
+<body>
+
+<h1>
+    Hello {{$name}}
+</h1>
+<section>
+    <p>
+        Hi {{$id}}
+    </p>
+</section>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}/{id}', function($name, $id) {
+    return view('hello', ["name" => $name, "id" => $id]);
+});
