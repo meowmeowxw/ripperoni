56aeacbda7d905fdb3e6b363b0e32b068a0568fb
Massimiliano Conti <massi@MacBook-Pro-di-Massimiliano.local>
2021-01-08 14:35:06 +0100

    Fix order component for better look

--- resources/views/components/user/customer-order.blade.php
+++ resources/views/components/user/customer-order.blade.php
@@ -1,16 +1,24 @@
-<div class="card m-1">
-    <div class="card-header">
-        Order Id:{{$order->id}}
+<div class="card m-1 ">
+    <div class="card-header d-flex flex-row">
+        <div class="mr-auto">Order Id: <b>{{$order->id}}</b></div>
+        <div class="ml-auto">Total Price: <b>{{$order->price}}€</b></div>
     </div>
     <div class="card-body">
-        <h4 class="card-title">Price:{{$order->price}}€</h4>
-        @foreach ($order->products as $beer)
-            <p class="card-text m-0">
 
-                {{ $beer->pivot->quantity }}x {{ $beer->pivot->price }}€ <b>{{ $beer->name }}</b> sell
-                by {{ \App\Models\Seller::find($beer->seller_id)->company }} <br>
-
-            </p>
-        @endforeach
+        {{--
+        <h4 class="card-title"> Price:{{$order->price}}€ </h4>
+        --}}
+        <div class="card-text">
+            @foreach ($order->products as $beer)
+                <div class="d-flex flex-row">
+                    <div class="align-self-center">
+                        <x-public.product :product=$beer :sellBy="TRUE"></x-public.product>
+                    </div>
+                    <div>
+                        <img src={{$beer->path}} class="card-img-top" alt="{{$beer->name}}"/>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -5,11 +5,9 @@
         <div class="col-md-8">
             <h1 class="text-center">Hey {{$user->name}}</h1>
             <h2>You have done {{ count($orders) }} orders</h2>
-            <div class="justify-content-center">
-                @foreach ($orders as $order)
-                    <x-user.customer-order :order=$order/>
-                @endforeach
-            </div>
+            @foreach ($orders as $order)
+                <x-user.customer-order :order=$order/>
+            @endforeach
         </div>
     </div>
 @endsection
