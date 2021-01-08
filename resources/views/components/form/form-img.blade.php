391e5c537ad3bcfaab45292c5fb7bb490c4225c6
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-08 15:51:47 +0100

    Use bootstrap select file:
    TODO: Fix select image

--- resources/views/components/form/form-img.blade.php
+++ resources/views/components/form/form-img.blade.php
@@ -11,10 +11,10 @@
 
 {{--
 Dobbiamo scegliere tra quello sopra e quello sotto pero se scegli commenta non cancellare please
---}}
 
 <x-form.form-div>
     <label for="{{ $attributes->get('idAndFor') }}" class="mb-0">{{ $attributes->get('lblValue') }}</label>
     <input id="{{ $attributes->get('idAndFor') }}" type="file" class="form-control-file" accept="image/*"
            @error('logo') is-invalid @enderror name="{{ $attributes->get('name') }}" value=""/>
 </x-form.form-div>
+--}}
