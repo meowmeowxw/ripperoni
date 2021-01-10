6b09a60f571b949f145774d6f9aac0c286f628ce
Maxxic <andmassic@icloud.com>
2021-01-10 22:17:13 +0100

    Add missing file of jquery for homepage

---
+++ public/js/homeselector.js
@@ -0,0 +1,11 @@
+$(".nav-link").click(function () {
+    $(this).parent().find("a.active").removeClass('active');
+    $(this).addClass('active');
+    let cat = $(this).text();
+    if(cat === "All"){
+        $(".filterDiv").show();
+    }
+    else {
+        $(".filterDiv").hide().filter('.' + cat).show();
+    }
+});
