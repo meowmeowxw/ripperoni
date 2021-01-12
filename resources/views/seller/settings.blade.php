7db2d5e17b0405f3385635409c9afbd71a528367
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-12 11:11:09 +0100

    Add change password to seller settings

--- resources/views/seller/settings.blade.php
+++ resources/views/seller/settings.blade.php
@@ -14,6 +14,19 @@
                     </x-form.form>
                 </div>
             </div>
+            <div class="col-md-8">
+                <h4 class="card-title">{{__('Change password')}}</h4>
+                <div class="card card-body">
+                    <x-form.form action="{{route('password.change')}}" btntext="{{ __('Save') }}"
+                                 btnaddclass="btn-lg btn-block">
+                        <x-FormInput name="password" idAndFor="password" :lblName="__('Old Password')" type="password"/>
+                        <x-FormInput name="new_password" idAndFor="new_password" :lblName="__('New Password')"
+                                     type="password"/>
+                        <x-FormInput name="new_password_confirmation" idAndFor="new_password_confirmation"
+                                     :lblName="__('Confirm New Password')" type="password"/>
+                    </x-form.form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
