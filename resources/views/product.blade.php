722f8e49da5d1edb41f645bb5bad62985ed54388
Maxxic <andmassic@icloud.com>
2021-01-12 14:56:45 +0100

    Init page product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,4 +4,29 @@
     <li>{{$product->name}}</li>
     <li>{{$seller->company}}</li>
     <li>{{$category->name}}</li>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="{{$product->path}}" class="m-1 img-fluid" alt=""/>
+            </div>
+            <div class="col-6">
+                <div class="card">
+                    <h3 class="card-title">gg</h3>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card card-body bg-transparent text-center">
+
+                    <x-form.form id="add_to_cart"
+                                 action="{{route('seller.product.add')}}"
+                                 enctype=""
+                                 btntext="{{__('Add to cart')}}" btnaddclass="btn-lg btn-block"
+                                 inputid="id_to_add" inputvalue="{{$product->id}}" >
+
+                        <x-FormInput name="quantity" idAndFor="quantityNew" inputValue="0" min="1" :lblName="__('Quantity')" type="number"/>
+                    </x-form.form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
