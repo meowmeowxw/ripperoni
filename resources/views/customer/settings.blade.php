5fbfce7c81f606043bcef3632bc5a594a246dc52
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-12 11:36:29 +0100

    Add component card and use it in {customer,seller} settings

--- resources/views/customer/settings.blade.php
+++ resources/views/customer/settings.blade.php
@@ -4,8 +4,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <h4 class="card-title">{{__('User settings')}}</h4>
-                <div class="card card-body">
+                <x-card>
+                    <x-slot name="title">
+                        {{__('User settings')}}
+                    </x-slot>
                     <x-form.form action="{{route('customer.settings')}}" btntext="{{ __('Save') }}"
                                  btnaddclass="btn-lg btn-block"
                                  inputid="type-user" name="type" inputvalue="user">
@@ -14,22 +16,26 @@
                         <x-FormInput name="email" idAndFor="email" :lblName="__('Email')"
                                      inputValue="{{Auth::user()->email}}" type="text"/>
                     </x-form.form>
-                </div>
+                </x-card>
             </div>
             <div class="col-md-8">
-                <h4 class="card-title">{{__('Customer settings')}}</h4>
-                <div class="card card-body">
+                <x-card>
+                    <x-slot name="title">
+                        {{__('Customer settings')}}
+                    </x-slot>
                     <x-form.form action="{{route('customer.settings')}}" btntext="{{ __('Save') }}"
                                  btnaddclass="btn-lg btn-block"
                                  inputid="type-customer" name="type" inputvalue="customer">
                         <x-FormInput name="credit_card" idAndFor="credit_card" :lblName="__('Credit Card')"
                                      inputValue="{{Auth::user()->customer->credit_card}}" type="text"/>
                     </x-form.form>
-                </div>
+                </x-card>
             </div>
             <div class="col-md-8">
-                <h4 class="card-title">{{__('Change password')}}</h4>
-                <div class="card card-body">
+                <x-card>
+                    <x-slot name="title">
+                        {{__('Change password')}}
+                    </x-slot>
                     <x-form.form action="{{route('password.change')}}" btntext="{{ __('Save') }}"
                                  btnaddclass="btn-lg btn-block">
                         <x-FormInput name="password" idAndFor="password" :lblName="__('Old Password')" type="password"/>
@@ -38,7 +44,7 @@
                         <x-FormInput name="new_password_confirmation" idAndFor="new_password_confirmation"
                                      :lblName="__('Confirm New Password')" type="password"/>
                     </x-form.form>
-                </div>
+                </x-card>
             </div>
         </div>
     </div>
--- resources/views/seller/settings.blade.php
+++ resources/views/seller/settings.blade.php
@@ -4,7 +4,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card card-body">
+                <x-card>
+                    <x-slot name="title">
+                        {{__('Settings')}}
+                    </x-slot>
                     <x-form.form action="{{route('seller.settings')}}" enctype="multipart/form-data"
                                  btntext="{{ __('Save') }}" btnaddclass="btn-lg btn-block">
                         <x-FormInput name="name" idAndFor="name" :lblName="__('Name')" inputValue="{{Auth::user()->name}}" type="text"/>
@@ -12,20 +15,22 @@
                         <x-FormInput name="company" idAndFor="company" :lblName="__('Company')" inputValue="{{Auth::user()->seller->company}}" type="text"/>
                         <x-FormInput name="credit_card" idAndFor="credit_card" :lblName="__('Credit Card')" inputValue="{{Auth::user()->seller->credit_card}}" type="text" />
                     </x-form.form>
-                </div>
+                </x-card>
             </div>
             <div class="col-md-8">
-                <h4 class="card-title">{{__('Change password')}}</h4>
-                <div class="card card-body">
-                    <x-form.form action="{{route('password.change')}}" btntext="{{ __('Save') }}"
-                                 btnaddclass="btn-lg btn-block">
-                        <x-FormInput name="password" idAndFor="password" :lblName="__('Old Password')" type="password"/>
-                        <x-FormInput name="new_password" idAndFor="new_password" :lblName="__('New Password')"
-                                     type="password"/>
-                        <x-FormInput name="new_password_confirmation" idAndFor="new_password_confirmation"
-                                     :lblName="__('Confirm New Password')" type="password"/>
-                    </x-form.form>
-                </div>
+                <x-card>
+                    <x-slot name="title">
+                        {{__('Change password')}}
+                    </x-slot>
+                        <x-form.form action="{{route('password.change')}}" btntext="{{ __('Save') }}"
+                                     btnaddclass="btn-lg btn-block">
+                            <x-FormInput name="password" idAndFor="password" :lblName="__('Old Password')" type="password"/>
+                            <x-FormInput name="new_password" idAndFor="new_password" :lblName="__('New Password')"
+                                         type="password"/>
+                            <x-FormInput name="new_password_confirmation" idAndFor="new_password_confirmation"
+                                         :lblName="__('Confirm New Password')" type="password"/>
+                        </x-form.form>
+                </x-card>
             </div>
         </div>
     </div>
