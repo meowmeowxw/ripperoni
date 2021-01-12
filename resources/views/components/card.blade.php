5aab1a427de7d7ed7b0bba1dab84631d9833a7ea
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-12 11:59:32 +0100

    Change card header color: TODO find a decent color

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,5 @@
 <div class="card">
-    <div class="card-header">{{ $title }}</div>
+    <div class="card-header text-center">{{ $title }}</div>
     <div class="card card-body">
         {{ $slot }}
     </div>
