bd27b2fa06cc42a4964e55a8d7edd21ffc4573c8
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-14 18:36:52 +0100

    SellerOrdersController fixed

--- app/Http/Controllers/Seller/SellerOrdersController.php
+++ app/Http/Controllers/Seller/SellerOrdersController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Seller;
 use App\Models\Seller;
 use App\Models\Product;
 use App\Models\Order;
+use App\Models\SellerOrder;
 use App\Providers\RouteServiceProvider;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -31,41 +32,15 @@ class SellerOrdersController extends Controller
     public function create()
     {
         $seller = Auth::user()->seller;
-        $orders = [];
+        $seller_orders = SellerOrder::where('seller_id', $seller->id)->get();
         $total_profit = 0;
-
-        /*
-         * Take order_id where there are seller products
-         */
-        $orders_id = DB::table('products')
-                    ->join('sub_orders', 'products.id', '=', 'sub_orders.product_id')
-                    ->select('sub_orders.order_id')
-                    ->where('seller_id', $seller->id)
-                    ->distinct()
-                    ->get()
-                    ->pluck('order_id')
-                    ->toArray();
-
-        /*
-         * Compute total earning
-         */
-        foreach ($orders_id as $order_id) {
-            $order = Order::find($order_id);
-            $products = $order->products()->where('seller_id', $seller->id)->get();
-            $profit = 0;
-            foreach ($products as $product) {
-                $profit += $product->pivot->total_price;
-            }
-            $total_profit += $profit;
-            $orders[] = ['id' => $order->id, 'products' => $products, 'profit' => $profit];
+        foreach ($seller_orders as $seller_order) {
+            $total_profit += $seller_order->profit;
         }
 
-        /*
-         * Return view
-         */
         return view('seller.orders', [
             'seller' => $seller,
-            'orders' => $orders,
+            'seller_orders' => $seller_orders,
             'total_profit' => $total_profit,
         ]);
     }
--- resources/views/seller/orders.blade.php
+++ resources/views/seller/orders.blade.php
@@ -5,15 +5,15 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <h3>{{__('Total Profit')}}: {{$total_profit}} &euro;</h3>
-                @foreach ($orders as $order)
-                <div class="card mt-3" id="customer-order.{{$order['id']}}">
+                @foreach ($seller_orders as $seller_order)
+                <div class="card mt-3" id="customer-order.{{$seller_order->order}}">
                     <div class="card-header d-flex flex-row">
-                        <div class="mr-auto">Order Id: <strong>{{$order['id']}}</strong></div>
-                        <div class="ml-auto">Total Price: <strong>{{$order['profit']}} &euro;</strong></div>
+                        <div class="mr-auto">Order Id: <strong>{{$seller_order->id}}</strong></div>
+                        <div class="ml-auto">Total Price: <strong>{{$seller_order->profit}} &euro;</strong></div>
                     </div>
                     <div class="card-body">
                         <div class="card-text">
-                            @foreach ($order['products'] as $beer)
+                            @foreach ($seller_order->products as $beer)
                                 <div class="row mt-4">
                                     <div class="col-sm-3">
                                         <a href="{{route('product.id', $beer->id)}}"><img src="{{$beer->path}}" class="card-img-top" alt="{{$beer->name}}"/></a>
