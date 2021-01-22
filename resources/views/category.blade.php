de1746ca900b03ebbf1331236e9c9db5c77c18c3
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-22 01:46:36 +0100

    Better jumbotron category page

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,26 +1,18 @@
 @extends('layouts.app')
 
-@section('styles')
-    <style>
-        .jumbotron {
-            background-color: #f4b0af;
-        }
-    </style>
-@endsection
-
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="jumbotron">
-                    <h3 class="text-center text-uppercase">{{$category->name}}</h3>
-                    <p>
+                    <h3 class="display-4 text-center text-uppercase border-bottom">{{$category->name}}</h3>
+                    <p class="lead">
                         {{$category->description}}
                     </p>
                 </div>
                 <div class="row row-cols-md-2 justify-content-center">
                     @foreach($products as $product)
-                        <x-product-square :product=$product />
+                        <x-product-square :product=$product/>
                     @endforeach
                 </div>
             </div>
