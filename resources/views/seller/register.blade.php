f5237b1aaba684928e62ee103ae4728ef6631e23
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2021-01-24 22:30:20 +0100

    Add class btn-block to btn forms

--- resources/views/seller/register.blade.php
+++ resources/views/seller/register.blade.php
@@ -85,7 +85,7 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-primary btn-block">
                                         {{ __('Register') }}
                                     </button>
                                 </div>
