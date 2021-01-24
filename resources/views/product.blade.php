985df068571884c7d41565952580ffc0f30254a9
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2021-01-24 20:00:24 +0100

    Change product view, add css for img

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('scripts')
     <script src="{{ asset('js/btn-product.js') }}" defer></script>
+    <link href="{{ asset('css/product.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
@@ -20,34 +21,50 @@
             </div>
             <div class="my-3 text-center">
                 <p class="h3 border-bottom">
-                    <a class="text-dark" href="{{route('seller.id', $seller->id)}}"> {{__('Company:')}} {{$seller->company}}</a>
+                    <a class="text-dark"
+                       href="{{route('seller.id', $seller->id)}}"> {{__('Company:')}} {{$seller->company}}</a>
                 </p>
             </div>
 
             <div class="row justify-content-center">
-                <div class="col-12 col-md-5 w-auto h-auto">
-                    <a>
-                        <img src="{{$product->path}}" class="w-75 m-1" alt=""/>
-                    </a>
+                <div class="col-12 col-md-5">
+                    <div class="product-img">
+                        <img src="{{$product->path}}" class="" alt="Beer {{$product->id ?? '0'}}"/>
+                    </div>
                 </div>
 
                 <div class="col-12 col-md-7">
                     <div class="col-12">
-
-                        <div class="my-3">
-                            <a class="btn-lg btn-dark"
-                               href="{{route('category.id', ['id' => $category->id])}}">  {{ $category->name }}  </a>
+                        <div class="row justify-content-center m-1">
+                            <p class="h4 ">
+                                {{$product->price}} €
+                            </p>
+                        </div>
+                        <div class="row justify-content-center m-1">
+                            <p class="h4">
+                            @if($product->isAvailable())
+                                ( {{$product->quantity}} {{__('in stock')}} )
+                            @else
+                                ({{__('Not in stocks')}} )
+                            @endif
+                            </p>
                         </div>
 
+                        <div class="my-3 jumbotron p-4">
+                            <h4 class="border-bottom text-center">
+                                    {{__('Description')}}:
+                            </h4>
+                            <p class="text-break small">{{$product->description}}</p>
+                        </div>
                     </div>
 
                     <div class="col-12 my-3">
                         <div class="card bg-transparent border-0 text-center">
-                            <div class="card-body">
+                            <div class="card-body m-0">
                                 {{--da fare if seller e if not seller per edit--}}
 
                                 @guest
-                                    <p class="small">Login needed to buy</p>
+                                    <p class="text-danger ">Login needed to buy</p>
                                 @else
                                     @if (Auth::user()->is_seller)
                                         <x-form.row-edit-product :product=$product></x-form.row-edit-product>
@@ -61,13 +78,38 @@
                 </div>
             </div>
 
-            <div class="my-3 jumbotron p-4">
-                <h3 class="border-bottom text-center">
-                    <strong>
-                        {{__('Description')}}:
-                    </strong>
-                </h3>
-                <p class="lead text-break">{{$product->description}}</p>
+            <div class="row justify-content-center mt-1">
+                <div class="col-12 col-md-6">
+                    <table id="details" class="table table-striped ">
+                        <caption class="h4">{{__('Details')}}:</caption>
+                        <tr>
+                            <th>
+                                {{__('Category')}}
+                            </th>
+                            <td>
+                                <a href="{{route('category.id', $category->id)}}">
+                                    {{$category->name}}
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{__('Format')}}
+                            </th>
+                            <td>
+                                {{$product->cl}} cl
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                {{__('Alcohol')}}
+                            </th>
+                            <td>
+                                {{$product->alcohol}} %
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
