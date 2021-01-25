cf2c50e1bc0f254d2ab89f62fb288d88cc1b4f63
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-25 12:46:57 +0100

    Refactor register and login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary btn-block">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <x-card>
+                    <x-slot name="title">
+                        {{ __('Register') }}
+                    </x-slot>
+                    <x-form.form action="{{ route('register') }}" btntext="{{ __('Register') }}"
+                                 btnaddclass="btn-block">
+                        <x-FormInput name="name" idAndFor="name" :lblName="__('Name')" type="text"
+                                     :value="old('name')"/>
+                        <x-FormInput name="email" idAndFor="email" :lblName="__('E-mail Address')" type="email"/>
+                        <x-FormInput name="password" idAndFor="password" :lblName="__('Password')" type="password"/>
+                        <label for="password-confirm"
+                               class="">{{ __('Confirm Password') }}</label>
+                        <input id="password-confirm" type="password" class="form-control mb-2"
+                               name="password_confirmation" required autocomplete="new-password"
+                               placeholder="{{__('Confirm Password')}}">
+                        <x-formInput name="credit_card" idAndFor="credit_card" :lblName="__('Credit Card')" type="numeric"/>
+                        <x-formInput name="street" idAndFor="street" :lblName="__('Street Address')" type="text"/>
+                        <x-formInput name="city" idAndFor="city" :lblName="__('City')" type="text"/>
+                    </x-form.form>
+                </x-card>
             </div>
         </div>
     </div>
-</div>
 @endsection
