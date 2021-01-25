d7ee57d44c0b474580929a1b9d398f8dea7d1e21
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2021-01-25 17:49:01 +0100

    Fix js

--- public/js/btn-product.js
+++ public/js/btn-product.js
@@ -1,11 +1,11 @@
-let quantity = parseInt($('#quantityCart').val());
-
 $("#btn-minus").click(function () {
+    let quantity = parseInt($('#quantityCart').val());
     if (quantity > 1) {
         $('#quantityCart').val(quantity - 1);
     }
 });
 
 $("#btn-plus").click(function () {
+    let quantity = parseInt($('#quantityCart').val());
     $('#quantityCart').val(quantity + 1);
 });
