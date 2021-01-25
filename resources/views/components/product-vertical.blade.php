6adbdb908839454e6e70e76799a6ea0cbbd5ae3f
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2021-01-25 21:08:17 +0100

    Make link dark

--- resources/views/components/product-vertical.blade.php
+++ resources/views/components/product-vertical.blade.php
@@ -1,9 +1,9 @@
 <div class="card bg-transparent col border-0 {{$class ?? ''}}">
 
-    <a href="{{route('product.id', $product->id)}}">
+    <a href="{{route('product.id', $product->id)}}" class="">
         <img class="card-img-top-xl" src="{{$product->path ?? '-'}}" alt="Card Beer {{$product->id+1 ?? '0'}}">
 
-        <div class="card-footer mt-auto">
+        <div class="card-footer mt-auto text-dark">
             <div class="card-title">
                 <strong>{{$product->name ?? '-'}}</strong>
             </div>
