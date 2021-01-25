bc6c71e761437201bb2b0bf37f185ddc503c5ea4
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2021-01-25 17:40:34 +0100

    Fix a link for all div

--- resources/views/components/product-vertical.blade.php
+++ resources/views/components/product-vertical.blade.php
@@ -1,16 +1,17 @@
 <div class="card bg-transparent col border-0 {{$class ?? ''}}">
 
-    <a href="{{route('product.id', ['id' => $product->id])}}">
+    <a href="{{route('product.id', $product->id)}}">
         <img class="card-img-top-xl" src="{{$product->path ?? '-'}}" alt="Card Beer {{$product->id+1 ?? '0'}}">
-    </a>
-    <div class="card-footer mt-auto">
-        <div class="card-title">
-            <strong>{{$product->name ?? '-'}}</strong>
+
+        <div class="card-footer mt-auto">
+            <div class="card-title">
+                <strong>{{$product->name ?? '-'}}</strong>
+            </div>
+            <p class="card-text">sell by
+                <strong>{{ \App\Models\Seller::where('id', $product->seller_id)->first()->company }}</strong>
+            </p>
+            {{--<b>{{ \App\Models\Seller::find($beer->seller_id)->company }}</b>--}}
         </div>
-        <p class="card-text">sell by
-            <strong>{{ \App\Models\Seller::where('id', $product->seller_id)->first()->company }}</strong>
-        </p>
-        {{--<b>{{ \App\Models\Seller::find($beer->seller_id)->company }}</b>--}}
-    </div>
+    </a>
 </div>
 
