5d12518611bc64419778943cfb692ba34da0bd5a
Massimiliano Conti <massimiliano.conti8@studio.unibo.it>
2021-01-25 15:39:39 +0100

    Fix row conflict

--- resources/views/components/product-square.blade.php
+++ resources/views/components/product-square.blade.php
@@ -1,4 +1,4 @@
-<div class="col-sm-6 col-md-4 services-box hoverServices p-1">
+<div class="col-sm-6 col-md-4 services-box hoverServices ">
     <a href="{{route('product.id', ['id' => $product->id])}}" class="text-dark">
         <p class="h4 text-center">{{ $product->name }}</p>
 
@@ -6,32 +6,35 @@
             <img src="{{$product->path ?? '-'}}"
                  alt="Card Beer {{$product->id+1 ?? '0'}}">
         </div>
+        <div class="bg-warning">
+            <div class="col justify-content-center">
+                <div class="row">
+                    @if ($product->isAvailable())
+                        <div class="col-3">
+                            <p class="font-weight-bold ">
+                                {{ $product->cl }} cl
+                            </p>
+                        </div>
+                        <div class="col-6">
+                            <p class="font-weight-bold ">
+                                {{ $product->alcohol }} % {{__('alcohol')}}
+                            </p>
+                        </div>
+                        <div class="col-3">
+                            <p class="font-weight-bold ">
+                                {{ $product->price }} &euro;
+                            </p>
+                        </div>
+                    @else
+                        <div class="col">
+                            <p class="font-weight-bold ">
+                                {{__('Not Available')}}
+                            </p>
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+
     </a>
-    <div class="container row">
-        @if ($product->isAvailable())
-            <div class="col-4">
-                <p class="font-weight-bold text-center">
-                    {{ $product->cl }} cl
-                </p>
-            </div>
-            <div class="col-4">
-                <p class="font-weight-bold text-center">
-                    {{ $product->alcohol }} % {{__('alcohol')}}
-                </p>
-            </div>
-            <div class="col-4">
-                <p class="font-weight-bold text-center">
-                    {{ $product->price }} &euro;
-                </p>
-            </div>
-        @else
-            <div class="col">
-                <p class="font-weight-bold text-center">
-                    {{__('Not Available')}}
-                </p>
-            </div>
-        @endif
-
-    </div>
-
 </div>
