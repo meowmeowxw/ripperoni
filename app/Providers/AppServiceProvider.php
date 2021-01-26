63dba78bb304f75034731b7f456fb5011d3155e7
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-26 17:45:11 +0100

    Use https on page links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(config('app.env') === 'production') {
+            $this->app['request']->server->set('HTTPS','on');
             \URL::forceScheme('https');
         }
         Paginator::useBootstrap();
