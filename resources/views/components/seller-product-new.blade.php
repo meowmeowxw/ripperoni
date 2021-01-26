9500e7b141efad76351a0feab37397c529b1d0bf
meowmeowxw <giovanni.disanti@protonmail.com>
2021-01-26 22:47:02 +0100

    Fix /seller/product/add on heroku

--- resources/views/components/seller-product-new.blade.php
+++ resources/views/components/seller-product-new.blade.php
@@ -6,7 +6,7 @@
         <x-form.form action="{{route('seller.product.add')}}" enctype="multipart/form-data"
                      btntext="{{ __('Save') }}" btnaddclass="btn-block">
 
-            <x-FormInput name="name" idAndFor="nameNew" :lblName="__('Name')" type="text" inputValue="{{old('name')}}"/>
+            <x-FormInput name="name" idAndFor="name" :lblName="__('Name')" type="text" inputValue="{{old('name')}}"/>
             <div class="form-row">
                 <div id="div-description" class="col">
                     <label for="description">{{__('Description')}}</label>
@@ -20,15 +20,18 @@
                     @enderror
                 </div>
             </div>
-            <x-FormInput name="price" idAndFor="priceNew" :lblName="__('Price')" type="number" step="0.01" inputValue="{{old('price')}}"/>
-            <x-FormInput name="quantity" idAndFor="quantityNew" :lblName="__('Quantity')" type="number" inputValue="{{old('quantity')}}" />
-            <x-FormInput name="alcohol" idAndFor="alcoholNew" :lblName="__('Alcohol level')" type="number"
+            <x-FormInput name="price" idAndFor="price" :lblName="__('Price')" type="number" step="0.01" inputValue="{{old('price')}}"/>
+            <x-FormInput name="quantity" idAndFor="quantity" :lblName="__('Quantity')" type="number" inputValue="{{old('quantity')}}" />
+            <x-FormInput name="alcohol" idAndFor="alcohol" :lblName="__('Alcohol level')" type="number"
                          step="0.01" inputValue="{{old('alcohol')}}"/>
             <x-FormInput name="cl" idAndFor="cl" :lblName="__('Cl')" type="number" inputValue="{{old('cl')}}"/>
 
             <x-form.form-category name="category" idAndFor="category" lblValue="Category"/>
 
-            <x-form.form-img name="logo" idAndFor="logoNew" lblValue="{{ __('Select Image') }}"/>
+            <x-form.form-img name="logo" idAndFor="logo" lblValue="{{ __('Select Image') }}"/>
+            <div class="form-row px-2">
+                <p class="small">{{__('Image max size 2.2 MB')}}</p>
+            </div>
         </x-form.form>
     </x-card>
 </div>
